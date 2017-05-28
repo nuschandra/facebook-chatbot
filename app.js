b2e11f0c09f5fa8e95f7c31f24286a6a252b10f2
Viswanathan <cviswana@visa.com>
2017-05-29 01:39:30 +0800

    adding verification for bot

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ var port=3000
 var fs=require('fs')
 
 app.get('/webhook',function(req,res){
-	res.send('Hello World!')
+	if (req.query['hub.mode'] === 'subscribe' &&
+		req.query['hub.verify_token'] === 'this_is_fun') {
+		console.log("Validating webhook");
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		console.error("Failed validation. Make sure the validation tokens match.");
+    	res.sendStatus(403);          
+  	}  
+  	res.send('Hello World!')
 })
 
 
