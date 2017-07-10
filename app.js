7414327bf35744630dcee4c55b756fc66222a93b
Viswanathan <cviswana@visa.com>
2017-07-11 01:18:22 +0800

    trying to get local time

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var port=3000;
 var fs=require('fs');
 var moment=require('moment');
 var config=require('config');
-require('moment/locale/en-gb')
+require('moment/locale/en')
 
 app.use(bodyParser.json());
 
@@ -178,6 +178,7 @@ function getCurrentMatches(callback){
 			var today=moment().format('LL');
 			console.log(today);
 			console.log(moment.locale());
+			console.log(moment().format('LT'));
 			var matches=body.data;
 			var todayMatches=matches.filter(function(match){
 				return (match.date===today);
